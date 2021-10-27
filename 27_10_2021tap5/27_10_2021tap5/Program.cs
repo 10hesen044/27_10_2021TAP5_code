a037ecd88f2d7832172fc80703136194943baea2
10hesen044 <tu201906250@code.edu.az>
2021-10-27 12:21:31 +0400

    project added

---
+++ 27_10_2021tap5/27_10_2021tap5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _27_10_2021tap5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 3, -5, -7, 2, -1 };
+            ChangeNegative(array);
+
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void ChangeNegative(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < 0)
+                {
+                    arr[i] *= -1;
+                }
+
+            }
+
+        }
+    }
+}
